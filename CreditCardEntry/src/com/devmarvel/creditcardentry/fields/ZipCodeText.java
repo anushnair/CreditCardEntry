3dda8bac46472453c04af7d43d5d07174bb8d324
anushnair <anushnair@gmail.com>
2021-05-19 18:03:12 -0500

    changing max chars for zipcode to 7 to accomodate CANADA

--- CreditCardEntry/src/com/devmarvel/creditcardentry/fields/ZipCodeText.java
+++ CreditCardEntry/src/com/devmarvel/creditcardentry/fields/ZipCodeText.java
@@ -29,8 +29,8 @@ public class ZipCodeText extends CreditEntryFieldBase {
 
 	void init() {
 		super.init();
-//		maxChars = 5;
-//		setMaxChars(maxChars);
+		maxChars = 7;
+		setMaxChars(maxChars);
 		setHint(R.string.ZipCodeFieldHint);
 	}
 
